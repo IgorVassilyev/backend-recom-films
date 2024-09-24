29bf78f00fd8ea1dd8d5e05a4d268a6f8d57a161
Vassilyev Igor <i.vassilyev8@gmail.com>
2024-09-24 19:13:59 +0500

    init project

---
+++ src/main/java/kz/vassilyev/web/recom_movies/RecommendationMoviesApplication.java
@@ -0,0 +1,13 @@
+package kz.vassilyev.web.recom_movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/vassilyev/web/recom_movies/RecomMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.vassilyev.web.recom_movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
